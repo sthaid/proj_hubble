62f6f40860d34017e5b0dc78ba2eb6778197d915
Steven Haid <stevenhaid@gmail.com>
2022-04-30 08:08:25 -0400

    continue

--- devel/sf3.c
+++ devel/sf3.c
@@ -11,16 +11,20 @@
 #define S_PER_BYR  (S_PER_YR * 1e9)
 #define S_PER_MYR  (S_PER_YR * 1e6)
 //#define LYR_PER_MPC  3.262e+6
-//#define M_PER_LYR   9.461e15
-//#define M_PER_BLYR   (M_PER_LYR * 1e9)
+#define M_PER_LYR   9.461e15
+#define M_PER_BLYR   (M_PER_LYR * 1e9)
 
 #define H_TO_SI (M_PER_KM / M_PER_MPC)
 
 double get_h(double t);
+double get_hsi(double t);
 
 double get_sf(double t);
 void init_sf(void);
 static double interpolate(double x, double x0, double x1, double y0, double y1);
+
+void trace_photon_back(void);
+void trace_photon_fwd(void);
 
 // -----------------  MAIN --------------------------------------------------
 
@@ -60,6 +64,16 @@ int main(int argc, char **argv)
             tbl[i].t, get_h(tbl[i].t), tbl[i].h);
     }
 
+    { double h, hsi;
+    h = get_h(13.8);
+    printf("h = %f  %e\n", h, h*H_TO_SI);
+    hsi = get_hsi(13.8);
+    printf("hsi = %e\n", hsi);
+    }
+
+    trace_photon_back();
+    //trace_photon_fwd();
+
     FILE *fpsf = fopen("filesf", "w");
     for (double t = 1; t < tmax; t += .001) {
         fprintf(fpsf, "%12.6f %12.6f\n", t, get_sf(t));
@@ -92,6 +106,11 @@ double get_h(double t)
     return h;
 }
 
+double get_hsi(double t)
+{
+    return get_h(t) * H_TO_SI;
+}
+
 // -----------------  SCALE FACTOR  -----------------------------------------
 
 double sf_tbl[20000];
@@ -114,14 +133,19 @@ double get_h_for_sf_init(double t);
 #define GET_START   6.3  //okay   GOOD
 #define GET_END    12.6
 
+#define USE_FIT
+
 // t in byrs
 double get_sf(double t)
 {
     double a;
 
+#ifdef USE_FIT
     if (t >= GET_START && t < GET_END) {
         a = get(t);
-    } else if (t < 9.8) {  // t >= .000380 && t < 9.8
+    } else 
+#endif
+    if (t < 9.8) {  // t >= .000380 && t < 9.8
         a = k1m + k2m * pow(t, 2./3.);
     } else if (t < 19.6) {  // t >= 9.8 && t < 19.6
         int idx;
@@ -191,8 +215,13 @@ double get_h_for_sf_init(double t)
         double t;
         double h;
     } tbl[] = {
+#ifdef USE_FIT
         { 9.8, 150 },    // 100
         { 10.9,115 },    //  90
+#else
+        { 9.8, 100 },    // 100
+        { 10.9,90 },    //  90
+#endif
         { 12.3, 80 },
         { 14.0, 70 },
         { 16.3, 60 },
@@ -229,3 +258,78 @@ static double interpolate(double x, double x0, double x1, double y0, double y1)
     result = y0 + (x - x0) * ((y1 - y0) / (x1 - x0));
     return result;
 }
+
+// xxxxxxxxxxxxxx
+
+void trace_photon_back(void)
+{
+    double t, x;
+
+    const double c = 3e8;
+
+    #define dt (t/1000)
+    #define h (get_hsi(t/S_PER_BYR))
+
+    double t_start_byr =
+        13.8;  // diameter should be 92
+        //24.5;  // diameter should be 200
+        //49.8;    // diameter should be 800
+
+    t = t_start_byr * S_PER_BYR;
+    x = 0;
+
+    FILE *fpdist = fopen("filedist", "w");
+    while (true) {
+        fprintf(fpdist, "%f   %f\n",
+            t / S_PER_BYR,
+            x / M_PER_BLYR);
+
+        x -= (c + h*x) * dt;
+
+        t -= dt;
+
+        if (t < .00038 * S_PER_BYR) break;
+        if (x >= 0) break;  // ?
+    }
+    fclose(fpdist);
+
+    double diameter;
+    diameter = (-x / M_PER_BLYR)  *
+               (get_sf(t_start_byr) / get_sf(t/S_PER_BYR)) *
+               2;
+    printf("A_START = %f \n", get_sf(t_start_byr));
+    printf("A_END   = %f \n", get_sf(t/S_PER_BYR));
+    printf("DIAMETER = %f\n", diameter);
+
+
+    #undef dt
+    #undef h
+}
+
+
+void trace_photon_fwd(void)
+{
+    double t, x;
+    const double c = 3e8;
+
+    #define dt (t/1000)
+    #define h (get_hsi(t/S_PER_BYR))
+
+    t = .00038 * S_PER_BYR;
+    x = -0.042349 * M_PER_BLYR;
+
+    while (true) {
+        printf("T = %f   X = %f\n",
+            t / S_PER_BYR,
+            x / M_PER_BLYR);
+        if (x >= 0) break;
+        if (t / S_PER_BYR > 1000) break;
+        x += (c + h * x) * dt;
+        t += dt;
+
+    }
+
+    #undef dt
+    #undef h
+}
+
