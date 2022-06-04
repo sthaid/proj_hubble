6cbd29d2a9df71e4937ed9d9c54aea5997de590c
Steven Haid <stevenhaid@gmail.com>
2022-06-04 08:57:28 -0400

    cont

--- cmb.c
+++ cmb.c
@@ -5,7 +5,13 @@
 // defines
 //
 
+#define FONT_SZ 20
 
+#define STATE_STR(s) \
+    ((s) == STOPPED ? "STOPPED" : \
+     (s) == RUNNING ? "RUNNING" : \
+     (s) == DONE    ? "DONE"    : \
+                      (assert(0),""))
 //
 // typdedefs
 // 
@@ -37,7 +43,7 @@ void sim_resume(void);
 
 void display_init(void);
 void display_hndlr(void);
-int graph_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_event_t * event);
+int main_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_event_t * event);
 
 // -----------------  MAIN  ---------------------------------------
   
@@ -46,7 +52,6 @@ int main(int argc, char **argv)
     // init
     sf_init();
     cmb_sim_init();
-while (1) pause();  // xxx let the thread run
     display_init();
 
     // runtime
@@ -64,10 +69,9 @@ void cmb_sim_init(void)
 
     // create the cmd_sim_thread
     pthread_create(&tid, NULL, cmb_sim_thread, NULL);
-
-    // xxx test, set state to running
-    state = RUNNING;
 }
+
+#define TEMPERATURE(_t)  (3000. * (get_sf(.00038) / get_sf(_t)))
 
 void * cmb_sim_thread(void *cx)
 {
@@ -81,8 +85,7 @@ void * cmb_sim_thread(void *cx)
 
         if (d == d_start) {
             double d_cmb_origion = d_start * get_sf(t) / get_sf(.000380);
-            double temperature = 3000 * (get_sf(.00038) / get_sf(t));
-            printf("START t=%f  d_photon=%f  d_cmb_origion=%f  temperature=%f\n", t, d, d_cmb_origion, temperature);
+            printf("START t=%f  d_photon=%f  d_cmb_origion=%f  temperature=%f\n", t, d, d_cmb_origion, TEMPERATURE(t));
         }
 
         // xxx comments
@@ -100,17 +103,21 @@ void * cmb_sim_thread(void *cx)
         if (d >= 0) {
             state = DONE;
             double d_cmb_origion = d_start * get_sf(t) / get_sf(.000380);
-            double temperature = 3000 * (get_sf(.00038) / get_sf(t));
-            printf("DONE t=%f  d_photon=%f  d_cmb_origion=%f  temperature=%f\n", t, d, d_cmb_origion, temperature);
+            printf("DONE t=%f  d_photon=%f  d_cmb_origion=%f  temperature=%f\n", t, d, d_cmb_origion, TEMPERATURE(t));
             continue;
         }
 
         // delay to adjust rate xxx 
+        static int count1;
+        if (++count1 == 30) {
+            usleep(100);
+            count1 = 0;
+        }
+
         static int count;
         if (count++ == 100000) {
             double d_cmb_origion = d_start * get_sf(t) / get_sf(.000380);
-            double temperature = 3000 * (get_sf(.00038) / get_sf(t));
-            printf("t=%f  d_photon=%f  d_cmb_origion=%f  temperature=%f\n", t, d, d_cmb_origion, temperature);
+            printf("t=%f  d_photon=%f  d_cmb_origion=%f  temperature=%f\n", t, d, d_cmb_origion, TEMPERATURE(t));
             count = 0;
         }
     }
@@ -118,11 +125,11 @@ void * cmb_sim_thread(void *cx)
 
 void sim_reset(void)
 {
-    state  = STOPPED;
+    state = STOPPED;
 
     t_done = 13.8;  // xxx make adjustable
     get_diameter(t_done, &d_start);
-    t = .000380;
+    t = .000380;  // xxx define
     d = d_start;
 }
 
@@ -160,6 +167,8 @@ void display_init(void)
     }
     INFO("REQUESTED win_width=%d win_height=%d\n", REQUESTED_WIN_WIDTH, REQUESTED_WIN_HEIGHT);
     INFO("ACTUAL    win_width=%d win_height=%d\n", win_width, win_height);
+
+    // xxx fail if size is wrong
 }
 
 void display_hndlr(void)
@@ -172,20 +181,23 @@ void display_hndlr(void)
         NULL,           // called after pane handlers
         100000,         // 0=continuous, -1=never, else us
         1,              // number of pane handler varargs that follow
-        graph_pane_hndlr, NULL,
-            0, 0, win_width, win_height,
+        main_pane_hndlr, NULL,
+            0, 0, win_height, win_height,
             PANE_BORDER_STYLE_MINIMAL
         );
 }
 
-int graph_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_event_t * event)
+int main_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_event_t * event)
 {
     struct {
         int nothing_yet;
     } * vars = pane_cx->vars;
     rect_t * pane = &pane_cx->pane;
 
-    #define SDL_EVENT_XXX        (SDL_EVENT_USER_DEFINED + 0)
+    #define SDL_EVENT_CTRL       (SDL_EVENT_USER_DEFINED + 0)
+
+    static int yellow[256];
+    int i;
 
     // ----------------------------
     // -------- INITIALIZE --------
@@ -195,6 +207,11 @@ int graph_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_e
         vars = pane_cx->vars = calloc(1,sizeof(*vars));
         INFO("PANE x,y,w,h  %d %d %d %d\n",
             pane->x, pane->y, pane->w, pane->h);
+
+        for (i = 0; i < 256; i++) {
+            yellow[i] = FIRST_SDL_CUSTOM_COLOR+i;
+            sdl_define_custom_color(yellow[i], i,i,0);
+        }
         return PANE_HANDLER_RET_NO_ACTION;
     }
 
@@ -203,7 +220,34 @@ int graph_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_e
     // ------------------------
 
     if (request == PANE_HANDLER_REQ_RENDER) {
-        sdl_render_printf(pane, 0, 0, 20, SDL_WHITE, SDL_BLACK, "HELLO");
+        int yidx;
+        double temperature = TEMPERATURE(t);
+        char *ctrl_str;
+
+        yidx = 30 + temperature * ((255. - 30) / 3000);
+        if (yidx < 0) yidx = 0;
+        if (yidx > 255) yidx = 255;
+
+        sdl_render_fill_rect(pane, &(rect_t){0,0,pane->w, pane->h}, yellow[yidx]);
+
+        ctrl_str = (state == STOPPED && t == .00038 ? "RUN"    :
+                    state == STOPPED                ? "RESUME" :
+                    state == RUNNING                ? "PAUSE"  :
+                    state == DONE                   ? "RESET"  :
+                                                      (assert(0), ""));
+
+        sdl_render_text_and_register_event(
+            pane, 0, 0, FONT_SZ, ctrl_str, SDL_LIGHT_BLUE, SDL_BLACK, 
+            SDL_EVENT_CTRL, SDL_EVENT_TYPE_MOUSE_CLICK, pane_cx);
+
+        sdl_render_printf(pane, 0, ROW2Y(1,FONT_SZ), FONT_SZ, SDL_WHITE, SDL_BLACK, 
+            "STATE = %s", 
+            STATE_STR(state));
+
+        sdl_render_printf(pane, 0, ROW2Y(3,FONT_SZ), FONT_SZ, SDL_WHITE, SDL_BLACK, 
+            "TIME=%0.6f  D=%0.6f  TEMP=%0.1f",
+            t, d, temperature);
+
         return PANE_HANDLER_RET_NO_ACTION;
     }
 
@@ -213,6 +257,15 @@ int graph_pane_hndlr(pane_cx_t * pane_cx, int request, void * init_params, sdl_e
 
     if (request == PANE_HANDLER_REQ_EVENT) {
         switch (event->event_id) {
+        case SDL_EVENT_CTRL:
+            if (state == STOPPED) {
+                sim_resume();
+            } else if (state == RUNNING) {
+                sim_pause();
+            } else {
+                sim_reset();
+            }
+            break;
         default: 
             break;
         }
